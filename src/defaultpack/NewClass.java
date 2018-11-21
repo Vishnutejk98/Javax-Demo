2dec7135500ffb29b9dd0a696e8616a0a3054511
vishnutejk98 <vishnutejk98@gmail.com>
2018-11-21 19:45:07 +0530

    loading files

---
+++ src/defaultpack/NewClass.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package defaultpack;
+
+/**
+ *
+ * @author whitehat
+ */
+public class NewClass {
+     public static void main(String[] args) {
+        lanuch screen = new lanuch();
+        //NewSignin sign = new NewSignin();
+        screen.setVisible(true);
+        try {
+            for (int row = 0; row <=10; row++) {
+                Thread.sleep(1000);
+                screen.loadnum.setText(Integer.toString(row)+"");
+                screen.load.setValue(row*10);
+                if (row == 10) {
+                    
+                    //screen.setVisible(false);
+                    //sign.setVisible(true);
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+}
